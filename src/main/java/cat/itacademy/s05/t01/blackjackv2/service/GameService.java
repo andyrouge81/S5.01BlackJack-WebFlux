ad0dd21d25ab218938139505dc434b7d0f71dea5
andyrouge81 <andyrouge81@gmail.com>
2026-01-25 17:45:42 +0100

    repo(game): add gameService and implement

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/service/GameService.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/service/GameService.java
@@ -1,4 +1,13 @@
 package cat.itacademy.s05.t01.blackjackv2.service;
 
+import cat.itacademy.s05.t01.blackjackv2.model.Game;
+import reactor.core.publisher.Mono;
+
 public interface GameService {
+
+    Mono<Game> createGame(Long playerId);
+
+    Mono<Game> findGameById(String gameId);
+
+    Mono<Game> play(String gameId);
 }
