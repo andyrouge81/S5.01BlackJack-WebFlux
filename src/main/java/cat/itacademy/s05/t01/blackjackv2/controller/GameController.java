9979a8fd58318343fe60d0c007f54fefb830899a
andyrouge81 <andyrouge81@gmail.com>
2026-01-28 13:52:22 +0100

    controller:refactor controllers

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/controller/GameController.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/controller/GameController.java
@@ -1,42 +1,58 @@
 package cat.itacademy.s05.t01.blackjackv2.controller;
 
-import cat.itacademy.s05.t01.blackjackv2.dto.CreateGameRequest;
-import cat.itacademy.s05.t01.blackjackv2.model.Game;
-import cat.itacademy.s05.t01.blackjackv2.service.GameServiceImpl;
+import cat.itacademy.s05.t01.blackjackv2.dto.request.CreateGameRequest;
+import cat.itacademy.s05.t01.blackjackv2.dto.request.PlayRequest;
+import cat.itacademy.s05.t01.blackjackv2.dto.response.GameResponse;
+import cat.itacademy.s05.t01.blackjackv2.mapper.GameMapper;
+import cat.itacademy.s05.t01.blackjackv2.service.game.GameServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "Blackjack Game")
 @RestController
 @RequestMapping("/game")
 public class GameController {
 
     private final GameServiceImpl gameService;
+    private final GameMapper mapper;
 
-    public GameController(GameServiceImpl gameService) {
+    public GameController(GameServiceImpl gameService, GameMapper mapper) {
+
         this.gameService = gameService;
+        this.mapper = mapper;
     }
 
     @PostMapping("/new")
-    public Mono<ResponseEntity<Game>> createGame(@RequestBody CreateGameRequest request) {
+    public Mono<ResponseEntity<GameResponse>> createGame(@RequestBody CreateGameRequest request) {
+
         return gameService.createGame(request.playerId())
-                .map(game->
-                    ResponseEntity.status(HttpStatus.CREATED).body(game));
+                .map(mapper::toResponse)
+                .map(dto ->
+                    ResponseEntity.status(HttpStatus.CREATED).body(dto));
 
     }
 
     @GetMapping("/{id}")
-    public Mono<ResponseEntity<Game>> getGameById(@PathVariable String gameId) {
+    public Mono<ResponseEntity<GameResponse>> getGameById(@PathVariable("id") String gameId) {
         return gameService.findGameById(gameId)
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+                .map(mapper::toResponse)
+                .map(ResponseEntity::ok);
     }
 
     @PostMapping("/{id}/play")
-    public Mono<ResponseEntity<Game>> play(@PathVariable String id) {
-        return gameService.play(id)
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<GameResponse> play(@PathVariable("id") String id, @RequestBody PlayRequest request) {
+        return gameService.play(id, request.action())
+                .map(mapper::toResponse);
+
+    }
+
+    @DeleteMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteGame(@PathVariable("id") String gameId) {
+        return gameService.deleteGame(gameId);
     }
 }
--- src/main/java/cat/itacademy/s05/t01/blackjackv2/controller/PlayerController.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/controller/PlayerController.java
@@ -1,4 +1,51 @@
 package cat.itacademy.s05.t01.blackjackv2.controller;
 
+import cat.itacademy.s05.t01.blackjackv2.dto.request.CreatePlayerRequest;
+import cat.itacademy.s05.t01.blackjackv2.dto.request.UpdatePlayerRequest;
+import cat.itacademy.s05.t01.blackjackv2.dto.response.PlayerGameResponse;
+import cat.itacademy.s05.t01.blackjackv2.dto.response.PlayerResponse;
+import cat.itacademy.s05.t01.blackjackv2.mapper.PlayerMapper;
+import cat.itacademy.s05.t01.blackjackv2.service.player.PlayerService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Tag(name = "Blackjack Player")
+@RestController
+@RequestMapping
 public class PlayerController {
+
+    private final PlayerService playerService;
+    private final PlayerMapper playerMapper;
+
+    public PlayerController(PlayerService playerService, PlayerMapper playerMapper) {
+
+        this.playerService = playerService;
+        this.playerMapper = playerMapper;
+    }
+    @PostMapping("/player")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<PlayerResponse> createPlayer(@RequestBody CreatePlayerRequest request) {
+        return playerService.createPlayer(request.playerName(), request.role())
+                .map(playerMapper::toResponse);
+    }
+    @PutMapping("/player/{playerId}")
+    public Mono<PlayerResponse> updatePlayerName(@PathVariable Long playerId,
+                                         @RequestBody UpdatePlayerRequest request) {
+        return playerService.updatePlayerName(playerId, request.playerName())
+                .map(playerMapper::toResponse);
+    }
+
+    @GetMapping("/ranking")
+    public Flux<PlayerResponse> getRanking() {
+
+        return playerService.getRanking()
+                .map(playerMapper::toResponse);
+    }
+    @GetMapping("player/{id}/games")
+    public Mono<PlayerGameResponse> getPlayerGame(@PathVariable Long id){
+        return playerService.getPlayerGames(id);
+    }
 }
