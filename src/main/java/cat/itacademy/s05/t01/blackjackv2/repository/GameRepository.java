b38395af67d154d2d0ddc7077e9d93b9c29447d6
andyrouge81 <andyrouge81@gmail.com>
2026-01-28 13:47:46 +0100

    repo: add new features in game and player repository

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/repository/GameRepository.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/repository/GameRepository.java
@@ -2,6 +2,9 @@ package cat.itacademy.s05.t01.blackjackv2.repository;
 
 import cat.itacademy.s05.t01.blackjackv2.model.Game;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface GameRepository extends ReactiveMongoRepository<Game, String> {
+
+    Flux<Game> findByPlayerId(Long playerId);
 }
--- src/main/java/cat/itacademy/s05/t01/blackjackv2/repository/PlayerRepository.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/repository/PlayerRepository.java
@@ -1,4 +1,11 @@
 package cat.itacademy.s05.t01.blackjackv2.repository;
 
-public interface PlayerRepository {
+import cat.itacademy.s05.t01.blackjackv2.model.Player;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+
+
+public interface PlayerRepository extends R2dbcRepository<Player, Long> {
+
+    Flux<Player> findAllByOrderByGamesWonDesc();
 }
