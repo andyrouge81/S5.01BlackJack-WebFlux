7233846dd1dca4b5ef58ba144bf255df6e81f153
andyrouge81 <andyrouge81@gmail.com>
2026-01-28 13:50:30 +0100

    config: add tags to swagger interface

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/config/OpenApiConfig.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/config/OpenApiConfig.java
@@ -1,4 +1,25 @@
 package cat.itacademy.s05.t01.blackjackv2.config;
 
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI blackjackOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Reactive BalckJack")
+                        .description("Reactive BlackJack game with MySQL and MongoDB")
+                        .version("2.0"));
+
+    }
 }
