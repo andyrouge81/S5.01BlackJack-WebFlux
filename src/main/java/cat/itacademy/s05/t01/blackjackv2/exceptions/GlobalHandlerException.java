02fd2030b37d864b3cc44d2dacc228ccede6be7d
andyrouge81 <andyrouge81@gmail.com>
2026-01-28 13:51:39 +0100

    exception:refactor globalHandler

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/exceptions/GlobalHandlerException.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/exceptions/GlobalHandlerException.java
@@ -1,4 +1,57 @@
 package cat.itacademy.s05.t01.blackjackv2.exceptions;
 
+import cat.itacademy.s05.t01.blackjackv2.dto.response.ApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
 public class GlobalHandlerException {
+
+    @ExceptionHandler(GameNotFoundException.class)
+    public ResponseEntity<ApiError> handlerGameNotFound(GameNotFoundException ex) {
+        ApiError error = new ApiError(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                Instant.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handlerGenericError(Exception ex) {
+        ApiError error = new ApiError(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<ApiError> handlerPlayerNotFound(PlayerNotFoundException ex) {
+        ApiError error = new ApiError(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                Instant.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiError> handleIllegalState(IllegalStateException ex) {
+        ApiError error = new ApiError(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                Instant.now()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
 }
--- src/main/java/cat/itacademy/s05/t01/blackjackv2/exceptions/PlayerNotFoundException.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/exceptions/PlayerNotFoundException.java
@@ -1,7 +1,8 @@
 package cat.itacademy.s05.t01.blackjackv2.exceptions;
 
 public class PlayerNotFoundException extends RuntimeException {
-  public PlayerNotFoundException(String message) {
-    super(message);
-  }
+    public PlayerNotFoundException(Long playerId) {
+
+        super("The player with id "+playerId+" not found");
+    }
 }
