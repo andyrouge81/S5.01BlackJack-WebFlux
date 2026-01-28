3eed9935ef789cf704deac644caca7bbe256310a
andyrouge81 <andyrouge81@gmail.com>
2026-01-28 13:51:09 +0100

    mapper:add game and player mappers

--- src/main/java/cat/itacademy/s05/t01/blackjackv2/mapper/GameMapper.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/mapper/GameMapper.java
@@ -1,4 +1,43 @@
 package cat.itacademy.s05.t01.blackjackv2.mapper;
 
+
+import cat.itacademy.s05.t01.blackjackv2.dto.response.CardResponse;
+import cat.itacademy.s05.t01.blackjackv2.dto.response.GameResponse;
+import cat.itacademy.s05.t01.blackjackv2.dto.response.HandResponse;
+import cat.itacademy.s05.t01.blackjackv2.model.Card;
+import cat.itacademy.s05.t01.blackjackv2.model.Game;
+import cat.itacademy.s05.t01.blackjackv2.model.Hand;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class GameMapper {
+
+    public GameResponse toResponse(Game game) {
+
+        return new GameResponse(
+                game.getId(),
+                game.getPlayerId(),
+                toHandResponse(game.getPlayerHand()),
+                toHandResponse(game.getDealerHand()),
+                game.getStatus(),
+                game.getResult()
+        );
+    }
+
+    private HandResponse toHandResponse(Hand hand) {
+
+        List<CardResponse> cards = hand.getCards().stream()
+                .map(this::toCardResponse)
+                .toList();
+
+        return new HandResponse(cards, hand.calculateValue());
+    }
+
+    private CardResponse toCardResponse(Card card) {
+
+        return new CardResponse(card.getSuit(), card.getRank());
+    }
+
 }
--- src/main/java/cat/itacademy/s05/t01/blackjackv2/mapper/PlayerMapper.java
+++ src/main/java/cat/itacademy/s05/t01/blackjackv2/mapper/PlayerMapper.java
@@ -1,4 +1,19 @@
 package cat.itacademy.s05.t01.blackjackv2.mapper;
 
+import cat.itacademy.s05.t01.blackjackv2.dto.response.PlayerResponse;
+import cat.itacademy.s05.t01.blackjackv2.model.Player;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PlayerMapper {
+
+    public PlayerResponse toResponse(Player player) {
+        return new PlayerResponse(
+                player.getId(),
+                player.getPlayerName(),
+                player.getRole(),
+                player.getGamesPlayed(),
+                player.getGamesWon()
+        );
+    }
 }
